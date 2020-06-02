32f53818bab419253ac0ae899862703f53447f02
ShimonMimoun <shimon1705@gmail.com>
2020-06-02 12:14:02 +0300

    Firt Commit

---
+++ Assets/Script/Colision.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("I am here");
+    }
+}
